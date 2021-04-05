a8984b825f1f3507f4f1d38005c17e3de754e7ba
xiaotinglian <36138129+xiaotinglian@users.noreply.github.com>
2021-04-05 11:35:21 -0400

    Add files via upload

--- lab7.scm
+++ lab7.scm
@@ -1,2 +1,2 @@
-(define (ftoc x) ( / (- x 32) ( / 9 5) ))
-(define (ctof y) ( + (* y ( / 9 5)) 32))
+(define (ftoc x) ( / (- x 32) ( / 9 5) ))   ;Fahrenheit to Celsius 
+(define (ctof y) ( + (* y ( / 9 5)) 32))	;Celsius to Fahrenheit
